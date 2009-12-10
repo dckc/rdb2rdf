65abd01349921a40b71d6361a99f56263354f544
eric@ubu <none@none>
2009-12-10 17:30:17 -0500

    + SQLbgp

--- src/test/scala/SparqlTest.scala
+++ src/test/scala/SparqlTest.scala
@@ -48,6 +48,35 @@ class SparqlTest extends FunSuite {
     assert(tps === a.parseAll(a.triplepatterns, e).get)
   }
 
+  test("SQLbgp") {
+    val a = Sparql()
+    val e = """
+SELECT ?empName ?manageName {
+?emp      <http://hr.example/DB/Employee#lastName>   ?empName .
+?emp      <http://hr.example/DB/Employee#manager>    ?manager .
+?manager  <http://hr.example/DB/Employee#lastName>   ?managName
+}
+"""
+    val tps =
+      SparqlSelect(
+	SparqlAttributeList(List(Var("empName"), Var("manageName"))),
+	TriplePatterns(
+	  List(
+	    TriplePattern(
+	      SVar(Var("emp")),
+		PUri(Stem("http://hr.example/DB"),Rel("Employee"),Attr("lastName")),
+	      OVar(Var("empName"))),
+	    TriplePattern(
+	      SVar(Var("emp")),
+	      PUri(Stem("http://hr.example/DB"),Rel("Employee"),Attr("manager")),
+	      OVar(Var("manager"))),
+	    TriplePattern(
+	      SVar(Var("manager")),
+	      PUri(Stem("http://hr.example/DB"),Rel("Employee"),Attr("lastName")),
+	      OVar(Var("managName"))))))
+    assert(tps === a.parseAll(a.select, e).get)
+  }
+
   test("decompose a predicate uri in stem, rel and attr") {
     val uri = "http://hr.example/DB/Employee#lastName"
     val puri:PUri = Sparql.parsePredicateURI(uri)
